4a57aad764559545d20f4b11ae90cc5b14309358
Enrique Perez <eaperezc@gmail.com>
2016-03-06 20:59:20 -0500

    to heroku

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -8,13 +8,13 @@ ActiveRecord\Config::initialize(function($cfg)
 {
     // The database connection string for the config
     $db_connection = sprintf('%s://%s:%s@%s/%s',
-        'mysql',        // db type
-        'root',         // username
-        'root',         // password
-        'localhost',    // host
-        'phpencil'      // schema
+        'pgsql',                                    // db type
+        'yuwqlpjqzssznq',                           // username
+        'scSZkmmX-yVZNRIpbJ6kN0s3KF',               // password
+        'ec2-54-83-12-22.compute-1.amazonaws.com',  // host
+        'dcvcglp892dht8'                            // schema
     );
-
+    
     // Here we set the configurations for the library
     $cfg->set_model_directory(APP_DIR . '/models');
     $cfg->set_connections(array('development' => $db_connection));
